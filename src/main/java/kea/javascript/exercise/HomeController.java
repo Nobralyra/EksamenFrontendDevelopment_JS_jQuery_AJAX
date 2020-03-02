78eb3bee6296c611d64d8b7c921805b94ebcedc3
Nobralyra <signees96@gmail.com>
2020-03-02 14:43:46 +0100

    First commit

---
+++ src/main/java/kea/javascript/exercise/HomeController.java
@@ -0,0 +1,15 @@
+package kea.javascript.exercise;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController
+{
+    @GetMapping("/")
+    public String HomePage ()
+    {
+        return "/js_basics";
+    }
+}
+
---
+++ src/test/java/kea/javascript/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,15 @@
+package kea.javascript.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
