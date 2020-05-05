870a4f89c590a706aa9493f9cd21474e7d0bfc6b
Nobralyra <signees96@gmail.com>
2020-05-05 19:16:58 +0200

    Added getmethod

--- src/main/java/kea/javascript/exercise/Controller/HomeController.java
+++ src/main/java/kea/javascript/exercise/Controller/HomeController.java
@@ -1,15 +1,19 @@
-package kea.javascript.exercise;
+package kea.javascript.exercise.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class HomeController
 {
     @GetMapping("/")
-    public String HomePage ()
+    public String homePage ()
     {
-        return "/js_basics";
+        return "js_basics_to_jquery";
+    }
+    @GetMapping("/ajax")
+    public String ajax ()
+    {
+        return "ajax";
     }
 }
 
